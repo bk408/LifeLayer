4da31925fa7dc55d8bf2450da7a1d1901977acab
Bhavya Khatri <bhavyakhatri1912@gmail.com>
2024-10-25 23:25:51 +0530

    lazy loading added

--- redux/src/App.jsx
+++ redux/src/App.jsx
@@ -2,34 +2,38 @@
 import { Provider } from "react-redux";
 import "./App.css";
 
-import AllProducts from "./components/AllProducts";
-import Contact from "./components/Contact";
-
 import Navbar from "./components/Navbar";
-import ProductBody from "./components/ProductBody";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import store from "./utils/store";
-import Cart from "./components/Cart";
 
 import { ToastContainer } from "react-toastify";
-import Success from "./components/Success";
-import Cancel from "./components/Cancel";
+
+import { lazy, Suspense } from "react";
+import Loading from "./components/Loading";
+
+const AllProducts = lazy(() => import("./components/AllProducts"));
+const Contact = lazy(() => import("./components/Contact"));
+const ProductBody = lazy(() => import("./components/ProductBody"));
+const Success = lazy(() => import("./components/Success"));
+const Cancel = lazy(() => import("./components/Cancel"));
+const Cart = lazy(() => import("./components/Cart"));
 
 function App() {
   return (
     <Provider store={store}>
       <Router>
         <Navbar />
+        <Suspense fallback={<Loading />}>
+          <Routes>
+            <Route path="/" element={<ProductBody />} />
+            <Route path="/all-products" element={<AllProducts />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/success" element={<Success />} />
+            <Route path="/cancel" element={<Cancel />} />
 
-        <Routes>
-          <Route path="/" element={<ProductBody />} />
-          <Route path="/all-products" element={<AllProducts />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/success" element={<Success />} />
-          <Route path="/cancel" element={<Cancel />} />
-
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </Suspense>
         <ToastContainer />
       </Router>
     </Provider>
